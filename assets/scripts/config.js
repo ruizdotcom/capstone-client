1784afefdd92b46ad73e2c64e6a484bbc7eac940
araceli-ruiz <ruizdotcom@gmail.com>
2020-12-24 13:30:47 -0500

    Deploy link

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://dashboard.heroku.com/apps/obscure-bayou-34723',
   development: 'http://localhost:4741'
 }
 
